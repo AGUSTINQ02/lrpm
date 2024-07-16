86dc053775fad50e81a60bd4c57504c880e1f3d0
AGUSTINQ02 <agustinquinones01@gmail.com>
2024-07-16 00:56:15 -0300

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "menu.h"
+#include "juego.h"
+#include "funciones.h"
+#include "../rlutil.h"
+using namespace std;
+
+void menu()
+{
+
+
+    int seleccion;
+    do
+    {
+        system ("cls");
+        rlutil::setColor(rlutil::RED);
+        rlutil::locate(54,4);
+        cout << "-------------------------" << endl;
+        rlutil::locate(54,5);
+        cout << "1. JUGAR MODO INDIVIDUAL " << endl;
+        rlutil::locate(54,6);
+        cout << "2. JUGAR MODO 2 JUGADORES " << endl;
+        rlutil::locate(54,7);
+        cout << "3. MODO SIMULACION " << endl;
+        rlutil::locate(54,8);
+        cout << "4. ESTADISTICAS " << endl;
+        rlutil::locate(54,9);
+        cout << "5. CREDITOS" << endl;
+        rlutil::locate(54,10);
+        cout << "------------------------" << endl;
+        rlutil::locate(54,11);
+        cout << "0. SALIR DEL JUEGO "<< endl;
+        rlutil::locate(54,12);
+        cout << "OPCION PARA SELECCIONAR: ";
+        cin >> seleccion;
+
+        switch (seleccion)
+        {
+
+        case 0:
+            return;
+            break;
+
+        case 1:
+            modo1jugador();
+
+            break;
+
+        case 2:
+            modo2jugadores();
+
+            break;
+
+        case 3:
+            modoPrueba1Jugador();
+
+            break;
+
+        case 4:
+
+
+            break;
+
+        case 5:
+            creditos();
+            break;
+
+
+        }
+
+
+    }
+
+
+    while(true);
+
+
+}
